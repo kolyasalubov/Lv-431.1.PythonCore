d01be8ab227e1da0afd4510df29b9b3880d4b730
Orikfw <Orik7800@gmail.com>
2019-08-03 18:13:38 +0300

    add some test\try

--- task1_28.py
+++ task1_28.py
@@ -1,7 +1,6 @@
-a = float(input("Введіть а\n"))
-b = float(input("Введіть b\n"))
-
 try:
+    a = float(input("Введіть число а\n"))
+    b = float(input("Введіть число b\n"))
     d = a + b
     m = a - b
     mul = a * b
@@ -9,5 +8,9 @@ try:
 except ZeroDivisionError:
     print(f"Сума: {d} \nРізниця: {m} \nМноження: {mul}")
     print('Виявлено ділення на нуль, операцію завершено...')
+except ValueError:
+    print("Ми тут працюємо з циферками...")
+except:
+    print("Ой, щось пішло не так...")
 else:
     print(f"Сума: {d} \nРізниця: {m} \nМноження: {mul} \nДілення: {dl}")
--- task2_28.py
+++ task2_28.py
@@ -1,5 +1,27 @@
-name = input('What is ur name?\n')
-age = int(input('How old are u?\n'))
-place = input('Where do u live?\n')
+def name_place_test(obj):
+    numb_count = 0
+    for i in obj:
+        if i == '0' or i == '1' or i == '2' or i == '3' or i == '4' or i == '5' or i == '6' or i == '7' or i == '8' or i == '9':
+            numb_count += 1
+    if numb_count > 0:
+        return False
+    return True
 
-print(f"Hello, {name} :) \nUr age is {age}. \nU live in {place}.")
+
+name = input('What is ur name?\n')
+if not name_place_test(name):
+    print('Ім\'я не може містити циферки...')
+    quit()
+else:
+    try:
+        age = int(input('How old are u?\n'))
+    except ValueError:
+        print('Вік потрібно вказати цифрами!')
+        quit()
+    else:
+        place = input('Where do u live?\n')
+        if not name_place_test(place):
+            print('Назва міста не може містити циферки...')
+            quit()
+        else:
+            print(f"Hello, {name} :) \nUr age is {age}. \nU live in {place}.")
