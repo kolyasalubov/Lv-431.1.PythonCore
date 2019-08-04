7776f9daef758610f5989ab1075b3d443ea2d7b8
“Arthur <arthurkoval.smn@gmail.com>
2019-08-04 22:51:31 +0300

    added new folder with hw

---
+++ HW/HW_01_2.py
@@ -0,0 +1,13 @@
+a = float(input("Введіть перше число "))
+b = float(input("Введіть друге число  "))
+
+ad = a+b
+sub = a-b
+mul = a*b
+div = a/b
+mod = a%b
+exp = a**b
+
+print (f"Сума: {ad} \nРізниця: {sub}\nМноження: {mul}\nДілення: {div}\nЗалишок від ділення: {mod}\nСтепінь: {exp}")
+
+    
