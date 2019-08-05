ed7a86a057cbfac9f1465c56d0c6b0132bfa615a
BogdanYatsiv <bogdanyatsiv@gmail.com>
2019-08-05 08:53:30 +0300

    Add my homework

---
+++ HW.py
@@ -0,0 +1,17 @@
+a = int(input("Enter a variable: "))
+b = int(input("Enter b variable: "))
+
+print("a + b =",a+b)
+print("a - b =",a-b)
+print("a x b =",a*b)
+print("a : b =",a/b)
+
+print("Now some questions\n")
+
+user_name = input("What is your name?\n")
+user_age = input("How old are you?\n")
+user_city = input("Where do you live?\n")
+
+print("Hello",user_name)
+print("Your age is",user_age)
+print("You live in",user_city)
